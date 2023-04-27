4e0519697d27d9cf7ed430b45980f3c57aba71c2
martin <martin-guyard@live.fr>
2023-04-27 11:43:12 +0200

    Add terms/privacy pages

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,37 @@
+import { Box } from "@chakra-ui/react";
+import { marked } from "marked";
+import { FC, useEffect, useState } from "react";
+import "./index.css";
+
+const Privacy: FC = () => {
+  const [md, setMd] = useState("");
+  const url =
+    "https://raw.githubusercontent.com/9OP/Encryptly/master/PRIVACY.MD";
+
+  useEffect(() => {
+    const fetchMd = async () => {
+      const res = await fetch(url);
+      const data = await res.text();
+      setMd(data);
+    };
+
+    fetchMd();
+  });
+
+  const getMarkdown = () => {
+    var rawMarkup = marked.parse(md);
+    return { __html: rawMarkup };
+  };
+
+  return (
+    <Box
+      padding="2rem"
+      width={{ base: "100%", md: "80%", lg: "70%", xl: "60%" }}
+      margin="auto"
+    >
+      <div dangerouslySetInnerHTML={getMarkdown()} />
+    </Box>
+  );
+};
+
+export default Privacy;
---
+++ src/pages/terms.tsx
@@ -0,0 +1,36 @@
+import { Box } from "@chakra-ui/react";
+import { marked } from "marked";
+import { FC, useEffect, useState } from "react";
+import "./index.css";
+
+const Terms: FC = () => {
+  const [md, setMd] = useState("");
+  const url = "https://raw.githubusercontent.com/9OP/Encryptly/master/TERMS.MD";
+
+  useEffect(() => {
+    const fetchMd = async () => {
+      const res = await fetch(url);
+      const data = await res.text();
+      setMd(data);
+    };
+
+    fetchMd();
+  });
+
+  const getMarkdown = () => {
+    var rawMarkup = marked.parse(md);
+    return { __html: rawMarkup };
+  };
+
+  return (
+    <Box
+      padding="2rem"
+      width={{ base: "100%", md: "80%", lg: "70%", xl: "60%" }}
+      margin="auto"
+    >
+      <div dangerouslySetInnerHTML={getMarkdown()} />
+    </Box>
+  );
+};
+
+export default Terms;
